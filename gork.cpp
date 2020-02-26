02b0f42fe642f6a685abe4cc1f5543a53e85bce8
天线无双 <1069911023@qq.com>
2020-02-26 19:40:15 +0800

    Add files via upload

---
+++ gork.cpp
@@ -0,0 +1,523 @@
+//
+//  gork.cpp
+//  OpenGLtest1
+//
+//  Created by apple on 2019/12/8.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#include "gork.hpp"
+#include "stb_image.h"
+
+
+////HDR
+//bool GORK::hdr = true;
+//bool GORK::hdrKeyPressed = false;
+//float GORK::exposure = 1.0f;
+
+//bloom
+bool GORK::bloom = Effect.BLOOM;  //bloom
+bool GORK::bloomKeyPressed = false;
+
+
+
+
+
+float GORK::deltaTime = 0.0f;
+float GORK::lastFrame = 0.0f;
+float GORK::lastX = SCR_WIDTH / 2.0f;
+float GORK::lastY = SCR_HEIGHT / 2.0f;
+bool GORK::firstMouse = true;
+
+
+
+
+// 在每次窗口大小被调整的时候被调用 glfw: whenever the window size changed
+// --------------------------------------
+void GORK::Framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height); //1,2参数控制窗口左下角的位置。3,4控制渲染窗口的宽度和高度（可以比GLFW的维度小）
+}
+
+// 在GLFW中实现一些输入控制：WSAD 和 esc
+// --------------------------------------
+void GORK::ProcessInput(GLFWwindow *window, float &nn, bool &nb)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //检查用户是否按下了返回键(Esc)
+    glfwSetWindowShouldClose(window, true); //GLFW被要求退出（渲染循环时检查）
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.ProcessKeyboard(RIGHT, deltaTime);
+
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !bloomKeyPressed)
+    {
+        bloom = !bloom;
+        bloomKeyPressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)
+    {
+        bloomKeyPressed = false;
+    }
+
+//    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+//    {
+//        if (exposure > 0.0f)
+//        exposure -= 0.001f;
+//        else
+//        exposure = 0.0f;
+//    }
+//    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+//    {
+//        exposure += 0.001f;
+//    }
+    
+//    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS && !nb)
+//    {
+////        bloom = !bloom;
+//        nn+=0.01;
+//        nb = true;
+//    }
+//    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_RELEASE)
+//    {
+//        nb = false;
+//    }
+//    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS && !nb)
+//    {
+//        //        bloom = !bloom;
+//        nn-=0.01;
+//        nb = true;
+//    }
+//    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_RELEASE)
+//    {
+//        nb = false;
+//    }
+}
+
+
+// 鼠标移动 glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void GORK::Mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset * 3, yoffset * 3);
+}
+
+// 鼠标滑轮 glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void GORK::Scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+
+GLFWwindow* GORK::InitWindow(){
+    // ----------------------------------
+    // 初始化和配置 glfw: initialize and configure
+    glfwInit(); //初始化GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  //使用 glfwWindowHint 函数来配置GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // //适配 OS X 的编译
+#endif
+    // ----------------------------------
+//    // 创建一个窗口对象 glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL); //创建窗口
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate(); //（Terminate终止） 释放/删除之前的分配的所有资源
+        return 0;
+    }
+    glfwMakeContextCurrent(window); //将创建的窗口设置为当前窗口（通知GLFW将我们窗口的上下文设置为当前线程的主上下文）
+    glfwSetFramebufferSizeCallback(window, GORK::Framebuffer_size_callback); //每当窗口调整大小的时候调用framebuffer_size_callback
+
+    // ----------------------------------
+//    // 鼠标控制
+//    glfwSetCursorPosCallback(window, GORK::Mouse_callback);//鼠标一移动mouse_callback函数就会被调用
+//    glfwSetScrollCallback(window, GORK::Scroll_callback);//滚轮一滚动scroll_callback函数就会被调用
+//
+//    // 捕捉鼠标 tell GLFW to capture our mouse
+//    //    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+//
+    // ----------------------------------
+    // 加载所有OpenGL函数指针 glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 0;
+    }
+    // ----------------------------------
+
+    return window;
+}
+
+
+
+
+//加载纹理贴图
+unsigned int GORK::LoadTexture(char const * pathFolder, char const * fileName, char const * suffix, bool isFlipped, bool isRepeated, bool gammaCorrection, TextureFiltering toSmall)//pathFolder文件夹路径，fileName文件名，suffix后缀
+{
+    char path[100];
+    std::strcpy(path, pathFolder);
+    std::strcat(path, fileName);
+    std::strcat(path, suffix);
+
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID); //生成 1 个纹理，保存ID到textureID
+
+    int width, height, nrComponents;
+
+    if(isFlipped)
+        stbi_set_flip_vertically_on_load(true); // 让stb_image.h在加载图片时翻转y轴
+
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0); //加载图片，得到长宽颜色通道信息
+    if (data)
+    {
+        GLenum internalFormat;
+        GLenum dataFormat;
+        if (nrComponents == 1)
+        {
+            internalFormat = dataFormat = GL_RED;
+        }
+        else if (nrComponents == 3)
+        {
+            internalFormat = gammaCorrection ? GL_SRGB : GL_RGB;
+            dataFormat = GL_RGB;
+        }
+        else {
+            internalFormat = gammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;
+            dataFormat = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID); // 绑定纹理，接下来所有GL_TEXTURE_2D操作都是对此纹理
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data); //生成一个纹理
+        //参数：纹理目标GL_TEXTURE_2D，Mipmap级别0，纹理存储为RGB格式，宽度，高度，历史遗留总是0，使用RGB值加载，储存为char(byte)数组，图像数据
+        glGenerateMipmap(GL_TEXTURE_2D); //自动生成所有需要的多级渐远纹理（Mipmap）
+
+        // 设置纹理环绕方式参数 set the texture wrapping parameters
+        if(isRepeated){
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        }else{
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);//超出的部分会重复纹理坐标的边缘
+            glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        }
+
+        switch (toSmall) {
+            case linear:
+            // 设置纹理过滤参数 set texture filtering parameters
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);//纹理被缩小的时候使用Mipmap
+            break;
+            case mipmap:
+            // 设置纹理过滤参数 set texture filtering parameters
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);//纹理被缩小的时候使用Mipmap
+            break;
+            case nearest:
+            // 设置纹理过滤参数 set texture filtering parameters
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//纹理被缩小的时候使用Mipmap
+            break;
+            
+            default:
+            break;
+        }
+        // 设置纹理过滤参数 set texture filtering parameters
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);//纹理被缩小的时候使用Mipmap
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//纹理被放大的时候使用线性过滤
+
+        stbi_image_free(data); //生成了纹理和相应的Mipmap后，释放图像的内存
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+
+}
+
+
+
+
+
+
+// loads a cubemap texture from 6 individual texture faces
+// order:
+// +X (right)// -X (left)// +Y (top)// -Y (bottom)// +Z (front)// -Z (back)
+//加载立方体贴图
+// -------------------------------------------------------
+unsigned int GORK::LoadCubemap(std::vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    stbi_set_flip_vertically_on_load(false); // 让stb_image.h在加载图片时翻转y轴
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
+
+
+// renders (and builds at first invocation) a sphere
+// -------------------------------------------------
+unsigned int sphereVAO = 0;
+unsigned int indexCount;
+void GORK::RenderSphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        unsigned int vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec3> normals;
+        std::vector<glm::vec2> uv;
+
+        std::vector<unsigned int> indices;
+
+        const unsigned int X_SEGMENTS = 64;
+        const unsigned int Y_SEGMENTS = 64;
+//        const float PI = 3.14159265359;
+        for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+            }
+        }
+
+        bool oddRow = false;
+        for (int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = indices.size();
+
+        std::vector<float> data;
+        for (int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 3 + 2) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+}
+
+
+// renderCube() renders a 1x1 3D cube in NDC.
+// -------------------------------------------------
+unsigned int cubeVAO = 0;
+unsigned int cubeVBO = 0;
+void GORK::RenderCube()
+{
+    // initialize (if necessary)
+    if (cubeVAO == 0)
+    {
+        float vertices[] = {
+            // back face
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+            1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right
+            1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+            // front face
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+            1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+            1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+            -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            // left face
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            // right face
+            1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+            1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+            1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right
+            1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+            1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+            1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // bottom face
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+            1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+            1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+            1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+            // top face
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+            1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+            1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right
+            1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+            -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left
+        };
+        glGenVertexArrays(1, &cubeVAO);
+        glGenBuffers(1, &cubeVBO);
+        // fill buffer
+        glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        // link vertex attributes
+        glBindVertexArray(cubeVAO);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+    }
+    // render Cube
+    glBindVertexArray(cubeVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+}
+
+
+
+// (屏幕缓冲)renderQuad() renders a 1x1 XY quad in NDC
+// -----------------------------------------
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void GORK::RenderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+            1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
+
+
+
+
+void GORK::SetupFramebuffer(unsigned int &captureFBO, unsigned int &captureRBO)
+{
+    glGenFramebuffers(1, &captureFBO);//帧缓冲
+    glGenRenderbuffers(1, &captureRBO);// 渲染缓冲对象。我们需要 深度 用于测试，但不需要对它们进行采样，所以渲染缓冲对象（只写）非常适合它们
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);//绑定帧缓冲对象
+    glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);//绑定渲染缓冲对象
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, 512, 512);//24位深度
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, captureRBO);// 把rbo附加到帧缓冲的深度附件上
+}
---
+++ gork.hpp
@@ -0,0 +1,244 @@
+//
+//  gork.hpp
+//  OpenGLtest1
+//
+//  Created by apple on 2019/12/8.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#ifndef gork_hpp
+#define gork_hpp
+
+#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <random>
+
+
+//#include "Shader.hpp"
+#include "Camera.hpp"
+//#include "Model.hpp"
+
+#include "gork.hpp"
+
+
+
+
+
+
+// 相机 camera
+//Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
+
+// settings 窗口宽和长
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+const bool Flip = true;//y轴翻转
+const bool NotFlip = false;
+const bool Repeat = true;//边缘重复
+const bool notRepeat = false;
+const bool sRGB = true;//是sRGB纹理，已经gammaCorrection
+const bool RGB = false;
+
+
+//typedef struct{
+//    ...
+//    ...
+//
+//} Material;
+//https://github.com/raysan5/raylib/blob/ea5cd42e6ccc3c0e520b98a2eb9ebc2c6ce62431/src/raylib.h
+//typedef enum {
+//    UNIFORM_FLOAT = 0,
+//    UNIFORM_VEC2,
+//    UNIFORM_VEC3,
+//    UNIFORM_VEC4,
+//    UNIFORM_INT,
+//    UNIFORM_IVEC2,
+//    UNIFORM_IVEC3,
+//    UNIFORM_IVEC4,
+//    UNIFORM_SAMPLER2D
+//} ShaderUniformDataType;
+//https://github.com/GORK44/raylib/blob/master/examples/models/models_material_pbr.c
+// Material type (generic)
+//typedef struct Material {
+//    Shader shader;          // Material shader
+//    MaterialMap *maps;      // Material maps array (MAX_MATERIAL_MAPS)
+//    float *params;          // Material generic parameters (if required)
+//} Material;
+//https://github.com/raysan5/raylib/blob/ea5cd42e6ccc3c0e520b98a2eb9ebc2c6ce62431/src/raylib.h
+
+
+
+static struct Effect{
+    bool IBL = 1;
+    bool BLOOM = 0;
+}Effect;
+
+typedef enum {
+    nearest = 0,          // Material shader
+    mipmap,
+    linear
+} TextureFiltering;
+
+//virtual void Fangfa(){
+//
+//}
+//多态性
+
+//析构函数？
+
+
+
+//类{
+//    get();
+//    set();
+//}
+
+
+
+
+//template<typename T>
+
+
+
+
+//#if defined(PLATFORM_DESKTOP)
+//mat.shader = LoadShader("resources/shaders/glsl330/pbr.vs", "resources/shaders/glsl330/pbr.fs");
+//#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
+//mat.shader = LoadShader("resources/shaders/glsl100/pbr.vs", "resources/shaders/glsl100/pbr.fs");
+//#endif
+
+#ifndef PI
+#define PI 3.14159265358979323846f
+#endif
+
+//#define DEG2RAD (PI/180.0f)
+//#define RAD2DEG (180.0f/PI)
+//
+//#define MAX_TOUCH_POINTS        10      // Maximum number of touch points supported
+//
+//// Allow custom memory allocators
+//#ifndef RL_MALLOC
+//#define RL_MALLOC(sz)       malloc(sz)
+//#endif
+//#ifndef RL_CALLOC
+//#define RL_CALLOC(n,sz)     calloc(n,sz)
+//#endif
+//#ifndef RL_FREE
+//#define RL_FREE(p)          free(p)
+//#endif
+
+
+
+//#pragma
+
+
+
+//内联(inline)函数
+
+
+
+//智能指针
+
+
+
+//浅拷贝与深拷贝
+
+
+
+// Some Basic Colors
+// NOTE: Custom raylib color palette for amazing visuals on WHITE background
+#define LIGHTGRAY  CLITERAL(Color){ 200, 200, 200, 255 }   // Light Gray
+#define GRAY       CLITERAL(Color){ 130, 130, 130, 255 }   // Gray
+#define DARKGRAY   CLITERAL(Color){ 80, 80, 80, 255 }      // Dark Gray
+#define YELLOW     CLITERAL(Color){ 253, 249, 0, 255 }     // Yellow
+#define GOLD       CLITERAL(Color){ 255, 203, 0, 255 }     // Gold
+#define ORANGE     CLITERAL(Color){ 255, 161, 0, 255 }     // Orange
+#define PINK       CLITERAL(Color){ 255, 109, 194, 255 }   // Pink
+#define RED        CLITERAL(Color){ 230, 41, 55, 255 }     // Red
+#define MAROON     CLITERAL(Color){ 190, 33, 55, 255 }     // Maroon
+#define GREEN      CLITERAL(Color){ 0, 228, 48, 255 }      // Green
+#define LIME       CLITERAL(Color){ 0, 158, 47, 255 }      // Lime
+#define DARKGREEN  CLITERAL(Color){ 0, 117, 44, 255 }      // Dark Green
+#define SKYBLUE    CLITERAL(Color){ 102, 191, 255, 255 }   // Sky Blue
+#define BLUE       CLITERAL(Color){ 0, 121, 241, 255 }     // Blue
+#define DARKBLUE   CLITERAL(Color){ 0, 82, 172, 255 }      // Dark Blue
+#define PURPLE     CLITERAL(Color){ 200, 122, 255, 255 }   // Purple
+#define VIOLET     CLITERAL(Color){ 135, 60, 190, 255 }    // Violet
+#define DARKPURPLE CLITERAL(Color){ 112, 31, 126, 255 }    // Dark Purple
+#define BEIGE      CLITERAL(Color){ 211, 176, 131, 255 }   // Beige
+#define BROWN      CLITERAL(Color){ 127, 106, 79, 255 }    // Brown
+#define DARKBROWN  CLITERAL(Color){ 76, 63, 47, 255 }      // Dark Brown
+
+#define WHITE      CLITERAL(Color){ 255, 255, 255, 255 }   // White
+#define BLACK      CLITERAL(Color){ 0, 0, 0, 255 }         // Black
+#define BLANK      CLITERAL(Color){ 0, 0, 0, 0 }           // Blank (Transparent)
+#define MAGENTA    CLITERAL(Color){ 255, 0, 255, 255 }     // Magenta
+#define RAYWHITE   CLITERAL(Color){ 245, 245, 245, 255 }   // My own White (raylib logo)
+
+
+class GORK {
+    
+public:
+    // 相机 camera
+//    Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
+    static Camera camera;
+
+    static float lastX;
+    static float lastY;
+    static bool firstMouse;
+    static float deltaTime; // 当前帧与上一帧的时间差
+    static float lastFrame; // 上一帧的时间
+    
+    
+    
+//    //HDR
+//    static bool hdr;
+//    static bool hdrKeyPressed;
+//    static float exposure;
+    
+    static bool bloom;  //bloom
+    static bool bloomKeyPressed;
+
+
+    
+
+
+    
+    static void Framebuffer_size_callback(GLFWwindow* window, int width, int height); // 在每次窗口大小被调整的时候被调用
+    static void ProcessInput(GLFWwindow *window, float &nn, bool &nb); //输入控制
+    static void Mouse_callback(GLFWwindow* window, double xpos, double ypos);//鼠标移动控制方向
+    static void Scroll_callback(GLFWwindow* window, double xoffset, double yoffset);//滚轮控制FOV
+    
+    
+    
+    static GLFWwindow* InitWindow();// 初始化和配置
+    
+    
+    static unsigned int LoadTexture(char const *pathFolder, const char *fileName, char const *suffix, bool isFlipped, bool isRepeated, bool gammaCorrection, TextureFiltering toSmall);//加载纹理贴图（反转，边缘重复，是sRGB纹理）(suffix后缀)
+//    static unsigned int LoadTexture(char const *pathFolder, const char *fileName, char const *suffix, bool isFlipped, bool isRepeated, bool gammaCorrection, bool mip);//加载纹理贴图（反转，边缘重复，是sRGB纹理）(suffix后缀)
+    static unsigned int LoadCubemap(std::vector<std::string> faces);//加载立方体贴图
+    
+    
+    //绘制几何体
+    static void RenderCube();
+    static void RenderQuad();
+    static void RenderSphere();
+
+    
+    static void SetupFramebuffer(unsigned int &captureFBO, unsigned int &captureRBO);
+    
+
+};
+
+
+
+
+
+
+
+
+#endif /* gork_hpp */
