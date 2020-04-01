4d308ec6de900c35f7559055499f4d46dfa5cca5
天线无双 <1069911023@qq.com>
2020-04-01 22:37:06 +0800

    Update gork.hpp

--- Source/gork.hpp
+++ Source/gork.hpp
@@ -26,11 +26,6 @@
 
 
 
-
-
-// 相机 camera
-//Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
-
 // settings 窗口宽和长
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -46,36 +41,6 @@ const bool RGB = false;
 
 
 
-
-
-//typedef struct{
-//    ...
-//    ...
-//
-//} Material;
-//https://github.com/raysan5/raylib/blob/ea5cd42e6ccc3c0e520b98a2eb9ebc2c6ce62431/src/raylib.h
-//typedef enum {
-//    UNIFORM_FLOAT = 0,
-//    UNIFORM_VEC2,
-//    UNIFORM_VEC3,
-//    UNIFORM_VEC4,
-//    UNIFORM_INT,
-//    UNIFORM_IVEC2,
-//    UNIFORM_IVEC3,
-//    UNIFORM_IVEC4,
-//    UNIFORM_SAMPLER2D
-//} ShaderUniformDataType;
-//https://github.com/GORK44/raylib/blob/master/examples/models/models_material_pbr.c
-// Material type (generic)
-//typedef struct Material {
-//    Shader shader;          // Material shader
-//    MaterialMap *maps;      // Material maps array (MAX_MATERIAL_MAPS)
-//    float *params;          // Material generic parameters (if required)
-//} Material;
-//https://github.com/raysan5/raylib/blob/ea5cd42e6ccc3c0e520b98a2eb9ebc2c6ce62431/src/raylib.h
-
-
-
 static struct Effect{
     bool IBL = 1;
     bool BLOOM = 0;
@@ -87,74 +52,12 @@ typedef enum {
     linear
 } TextureFiltering;
 
-//virtual void Fangfa(){
-//
-//}
-//多态性
-
-//析构函数？
-
-
-
-//类{
-//    get();
-//    set();
-//}
-
-
-
-
-//template<typename T>
-
-
-
-
-//#if defined(PLATFORM_DESKTOP)
-//mat.shader = LoadShader("resources/shaders/glsl330/pbr.vs", "resources/shaders/glsl330/pbr.fs");
-//#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
-//mat.shader = LoadShader("resources/shaders/glsl100/pbr.vs", "resources/shaders/glsl100/pbr.fs");
-//#endif
 
 #ifndef PI
 #define PI 3.14159265358979323846f
 #endif
 
-//#define DEG2RAD (PI/180.0f)
-//#define RAD2DEG (180.0f/PI)
-//
-//#define MAX_TOUCH_POINTS        10      // Maximum number of touch points supported
-//
-//// Allow custom memory allocators
-//#ifndef RL_MALLOC
-//#define RL_MALLOC(sz)       malloc(sz)
-//#endif
-//#ifndef RL_CALLOC
-//#define RL_CALLOC(n,sz)     calloc(n,sz)
-//#endif
-//#ifndef RL_FREE
-//#define RL_FREE(p)          free(p)
-//#endif
 
-
-
-//#pragma
-
-
-
-//内联(inline)函数
-
-
-
-//智能指针
-
-
-
-//浅拷贝与深拷贝
-
-
-
-// Some Basic Colors
-// NOTE: Custom raylib color palette for amazing visuals on WHITE background
 #define LIGHTGRAY  CLITERAL(Color){ 200, 200, 200, 255 }   // Light Gray
 #define GRAY       CLITERAL(Color){ 130, 130, 130, 255 }   // Gray
 #define DARKGRAY   CLITERAL(Color){ 80, 80, 80, 255 }      // Dark Gray
@@ -298,10 +201,6 @@ public:
     
     
 };
-
-
-
-
 
 
 
