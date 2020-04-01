8b0c4762b1f8f2cc803604e44b748bce5acf78e3
天线无双 <1069911023@qq.com>
2020-04-01 22:33:45 +0800

    Update Material.hpp

--- Source/Material.hpp
+++ Source/Material.hpp
@@ -24,11 +24,6 @@ typedef enum {
 
 
 
-//=========================================================
-//改成虚基类，实现多态！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！/
-//pbr 和 普通 的 material
-//=========================================================
-
 class Material
 {
 private:
@@ -77,36 +72,6 @@ public:
     void BindShaderTexture()
     {
         if (Effect.IBL) {
-            // 材质球
-            // 绑定PBR材质贴图（shader之后会调用）//前三张图是irradianceMap，prefilterMap，brdfLUTTexture
-            
-            //    GORK::ShaderSet_PBR_Ball(pbrShader_redBall);
-            ////    pbrShader_redBall.use();
-            ////    pbrShader_redBall.setInt("irradianceMap", 0);
-            ////    pbrShader_redBall.setInt("prefilterMap", 1);
-            ////    pbrShader_redBall.setInt("brdfLUT", 2);
-            ////    pbrShader_redBall.setVec3("albedo", 1.0f, 1.0f, 1.0f);
-            ////    pbrShader_redBall.setFloat("ao", 1.0f);
-            ////
-            ////    pbrShader_redBall.setInt("noiseMap", 3);
-            //
-            //
-            //
-            //
-            //    GORK::ShaderSet_PBR_Model(pbrShader);
-            ////    pbrShader.use();
-            ////    pbrShader.setInt("irradianceMap", 0);// shader中的 irradianceMap 对应 GL_TEXTURE0 绑定的纹理
-            ////    pbrShader.setInt("prefilterMap", 1);// 预滤波贴图
-            ////    pbrShader.setInt("brdfLUT", 2); // LUT贴图
-            ////    pbrShader.setInt("albedoMap", 3); // 反照率（基础颜色）贴图
-            ////    pbrShader.setInt("normalMap", 4); // 法线贴图
-            ////    pbrShader.setInt("metallicMap", 5); // 金属度贴图
-            ////    pbrShader.setInt("roughnessMap", 6); // 粗糙度贴图
-            ////    pbrShader.setInt("aoMap", 7); // 环境光遮蔽贴图
-            ////
-            ////    pbrShader.setInt("noiseMap", 8);
-            //
-            
             glActiveTexture(GL_TEXTURE3);
             glBindTexture(GL_TEXTURE_2D, albedoMap);
             glActiveTexture(GL_TEXTURE4);
@@ -132,17 +97,7 @@ public:
     }
     
     
-    
-    
-    
-    
 };
-
-
-
-
-
-
 
 
 
